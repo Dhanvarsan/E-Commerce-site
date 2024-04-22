ac695d1a5a3ffb2ecf5598af5bfe9bb543f4c39e
Dhanvarsan Selvaraj <106432385+Dhanvarsan@users.noreply.github.com>
2024-04-22 12:05:30 +0530

    minor changes

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  // publicPath:'/E-Commerce-site/',
+  publicPath:'/E-Commerce-site/',
   transpileDependencies: true
 })
